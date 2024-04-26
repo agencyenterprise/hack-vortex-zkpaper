6dceb67c54d678a7cf6cf74e79880b037bf0ed7b
lolo <lolo.tomaz@gmail.com>
2024-04-26 00:04:49 -0300

    added wallet integration

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 export default defineConfig({
   optimizeDeps: {
     esbuildOptions: {
       target: 'esnext',
     },
   },
+
   build: {
     target: 'esnext',
   },
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
+
 });
